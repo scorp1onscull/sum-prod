7b1c627faad6eb791073f11ee2f01796a166e7ca
st142661 <bomboclat@mail.ru>
2025-10-21 12:51:56 +0300

    feat: command line

--- fimoz.cpp
+++ fimoz.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
-int main(){
+int main(int argc, char* argv[]){
+	unsigned int number;
+        unsigned int sum = 0;
+        unsigned int mult = 1;
+	if (argc>1){
+		char*number = argv[1];
+		int index = 0;
+		char ch;
+		while( number[index] != '\0' ){
+			if ( number[index] >= '0' && number[index] <= '9'){
+				sum += number[index];
+				mult *= number[index];
+			} else {
+				std::cout << "wrong number" << argv[1] << " " << std::endl;
+			}
+		}
+		std::cout << sum << ", " << mult << std::endl;
+	} else {
 	unsigned int x;
 	unsigned int sum = 0;
 	unsigned int mult = 1;
@@ -11,4 +28,5 @@ int main(){
 	}while (x > 0);
 	std::cout << sum << ", " << mult << std::endl;
 	return 0;
+	}
 }
